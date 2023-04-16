c56f3d5a6a8acf5bcc8c658a05ab37dbb27979cb
joswaim <joswaim@gmail.com>
2023-04-15 17:56:33 -0700

    test_environment was used to determine if different sections of code would return an array correctly

---
+++ test_environment.rb
@@ -0,0 +1,48 @@
+# def string_lengths(sentence)
+#   strings = sentence.split
+
+#   strings.map { |chars| chars.length }
+# end
+
+
+# def string_lengths(sentence)
+#   strings = sentence.split
+#   lengths = []
+
+#   strings.each do |string|
+#     lengths << string.size
+#   end
+#   # return lengths -> this would fix this code, it returns strings
+# end
+
+
+# def string_lengths(sentence)
+#   words = sentence.split
+#   word_lengths = []
+#   counter = 0
+
+#   while counter < words.size do
+#     word_lengths << words[counter].length
+#     counter += 1
+#   end
+
+#   word_lengths
+# end
+
+
+# def string_lengths(sentence)
+#   strings = sentence.split
+#   lengths = []
+#   counter = 1
+
+#   until counter == strings.size do
+#     word_length = strings[counter - 1].length
+#     lengths.push(word_length)
+#     counter += 1
+#   end
+
+#   lengths
+# end
+
+
+p string_lengths('to be or not to be')
